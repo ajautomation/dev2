4c6ed715fd60492bd27059edaa5431d57efcc3fe
AJ <86994382+ajautomation@users.noreply.github.com>
2023-05-04 20:54:16 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is a sample Java Project MyApp");
+
+		//suggested code changes are done for for loop
+		//change 2
+
+		//Direct commit from AJ
+	}
+
+}
